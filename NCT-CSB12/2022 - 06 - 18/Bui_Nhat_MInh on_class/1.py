4884530b4de2bbdb7fc1271418cba83e7bd8c5ed
MawatariTanaka <buinhatminh2005@gmail.com>
2022-06-24 10:36:30 +0700

    Update lab10

--- NCT-CSB12/2022 - 06 - 18/Bui_Nhat_MInh on_class/1.py
+++ NCT-CSB12/2022 - 06 - 18/Bui_Nhat_MInh on_class/1.py
@@ -2,6 +2,9 @@ keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
 
 # Từ điển cần trả về
-dictionary = dict(zip(keys, values))
+# dictionary = dict(zip(keys, values))
+dictionary = {}
+for i in range(len(keys)):
+    dictionary[keys[i]] = values[i]
 
 print(dictionary)
